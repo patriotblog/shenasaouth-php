2c43619aca83ff5791ad3f17e47886551d0cf8ce
patriot <taebi.ali@gmail.com>
2013-09-11 13:49:49 +0430

    validate HTML

--- index.php
+++ index.php
@@ -23,10 +23,17 @@
 	}
 	
 ?>
-
-<?php if(isset($_COOKIE['user_email'])) :?>
-	<a href="?logout"><?php echo $_COOKIE['user_email'];?> (logout)</a>
-	<pre><?php print_r($oAuth->getUserInfo()); ?></pre>
-<?php else:?>
-	<a href="<?php echo $oAuth->generateLoginUri();?>">login</a>
-<?php endif;?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset='utf-8'>
+	</head>
+	<body>
+		<?php if(isset($_COOKIE['user_email'])) :?>
+			<a href="?logout"><?php echo $_COOKIE['user_email'];?> (logout)</a>
+			<pre><?php print_r($oAuth->getUserInfo()); ?></pre>
+		<?php else:?>
+			<a href="<?php echo $oAuth->generateLoginUri();?>">login</a>
+		<?php endif;?>
+	</body>
+</html>
